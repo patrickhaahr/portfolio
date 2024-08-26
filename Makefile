dd41762efc7c13e3d7793d5033bc434f4463de4d
patrickhaahr <patrickqhaahr@gmail.com>
2024-08-26 07:27:58 +0200

    Intial

---
+++ Makefile
@@ -0,0 +1,11 @@
+run: build
+	@./bin/app
+
+build:
+	@go build -o bin/app .
+
+css:
+	tailwindcss -i views/css/app.css -o public/styles.css --watch
+
+templ:
+	templ generate --watch --proxy=http://localhost:3000
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"GOTH/handlers"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+	router := chi.NewMux()
+
+	router.Handle("/*", public())
+	router.Get("/", handlers.Make(handlers.HandleHome))
+
+	listenAddr := os.Getenv("LISTEN_ADDR")
+	slog.Info("HTTP server started", "listenAddr", listenAddr)
+	http.ListenAndServe(listenAddr, router)
+}
