1b336ce38ae6ba97f150849ff4b2a582c9a7a6ff
patrickhaahr <patrickqhaahr@gmail.com>
2024-09-02 13:13:43 +0200

    UI changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
   darkMode: 'selector',
   content: ["./**/*.html", "./**/*.templ", "./**/*.go",],
   safelist: [],
-
+  extend: {
+    padding: {
+      '64-important': '16rem !important',
+    },
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.py-64-important': {
+          paddingTop: '16rem !important',
+          paddingBottom: '16rem !important',
+        },
+      });
+    },
+  ],
 }
 
